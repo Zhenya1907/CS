aaddd0ebc6c930b16c47af342b0011711361925c
Zhenya <e.group550@gmail.com>
2022-06-16 13:01:57 +0400

    task_2

---
+++ ConsoleApp2_2/Program_2.cs
@@ -0,0 +1,29 @@
+﻿int left = 1;
+int right = 100;
+
+while (true)
+{
+    int carrent = (left + right) / 2;
+    Console.WriteLine($"Is your number {carrent}? (yes, bigger, smaller)");
+    var is_correct = Console.ReadLine();
+
+
+    if (is_correct == "yes")
+    {
+        Console.WriteLine("Olala!!!");
+        break;
+    }
+    else if (is_correct == "bigger")
+    {
+        left = carrent + 1;
+    }
+    else if (is_correct == "smaller")
+    {
+        right = carrent - 1;
+    }
+    else
+    {
+        Console.WriteLine("Don't understand you");
+    }
+
+}
